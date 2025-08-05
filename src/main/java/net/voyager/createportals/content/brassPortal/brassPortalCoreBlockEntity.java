a129795d467181e0896dd72943292553f86a127b
Voyager <139731885+Viking-Voyager148@users.noreply.github.com>
2025-08-04 20:34:49 -0500

    insertion + extraction

--- src/main/java/net/voyager/createportals/content/brassPortal/brassPortalCoreBlockEntity.java
+++ src/main/java/net/voyager/createportals/content/brassPortal/brassPortalCoreBlockEntity.java
@@ -22,7 +22,7 @@ public class brassPortalCoreBlockEntity extends KineticBlockEntity implements IH
     private static final int MAX_FLUID = 8000;
 
     SmartFluidTankBehaviour tank;
-    private final List<String> portalLinks = new ArrayList<>Collections.nCopies(MAX_LINKS, "");
+    private final List<String> portalLinks = new ArrayList<>(Collections.nCopies(MAX_LINKS, ""));
     private int selectedIndex = 4;
     private String portalName = "";
     private boolean isNetworkLocked = false;
@@ -35,13 +35,22 @@ public class brassPortalCoreBlockEntity extends KineticBlockEntity implements IH
     @Override
     protected void addBehaviours(List<BlockEntityBehaviour> behaviours) {
         tank = SmartFluidTankBehaviour.single(this, MAX_FLUID);
-        tank.forbidExtraction();
-        tank.allowExtraction(direction, fluidStack) -> {
+            tank.allowInsertion((Direction direction, FluidStack fluidStack) -> {
+            if (direction == null) return false;
+
+            Direction blockFacing = getBlockState().getValue(HorizontalDirectionalBlock.FACING);
+            boolean correctSide = direction == blockFacing.getOpposite();
+            boolean isEnderFluid = fluidStack.getFluid().isSame(ModFluids.ENDER_FLUID.get());
+
+            return correctSide && isEnderFluid;
+            return direction == blockFacing.getOpposite();
+        });
+        tank.allowExtraction((Direction direction, FluidStack fluidStack) -> {
             if (direction == null) return false;
 
             Direction blockFacing = getBlockState().getValue(HorizontalDirectionalBlock.FACING);
             return direction == blockFacing.getOpposite();
-                };
+        });
         behaviours.add(tank);
     }
 }
